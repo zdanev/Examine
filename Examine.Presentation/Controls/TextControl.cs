450bccc26e06d24cfc8cc991b1f9ca34936ce26c
Zdravko Danev <zdanev@gmail.com>
2018-02-19 17:05:28 -0800

    added text control, refactored view

---
+++ Examine.Presentation/Controls/TextControl.cs
@@ -0,0 +1,22 @@
+using Examine.Presentation.Tags;
+using static Examine.Presentation.Tags.Fluent;
+
+namespace Examine.Presentation.Controls
+{
+    public class TextControl : BodyElement
+    {
+        public string Id { get; set; }
+
+        public string Label { get; set; }
+
+        public TextControl(string id, string name, string label) : base("div")
+        {
+            Id = id;
+            Label = label;
+
+            this.Class("form-group");
+            this.Content.Add(Label(label).Attr("label-for", id));
+            this.Content.Add(Input(InputType.Text).Id(id).Attr("name", name).Class("form-control"));
+        }
+    }
+}
--- Examine.Presentation/Controls/View.cs
+++ Examine.Presentation/Controls/View.cs
@@ -1,11 +1,24 @@
+using System.Linq;
+using System.Text;
 using Examine.Presentation.Tags;
 
 namespace Examine.Presentation.Controls
 {
-    public abstract class View : BodyElement
+    public abstract class View : Tag, IBodyElement // BodyElement
     {
-        public View(params IBodyElement[] content) : base("div", content)
+        public View(params IBodyElement[] content) : base("", content)
         {
+        }
+
+        public override void RenderTo(StringBuilder sb)
+        {
+            if (Content.Any())
+            {
+                foreach (var element in Content)
+                {
+                    element.RenderTo(sb);
+                }
+            }
         }
     }
 }
